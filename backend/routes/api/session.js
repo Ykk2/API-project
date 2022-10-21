fc6dc85d86859ba34e98cf22da67dec6c1921c14
Richard <richardkwon2@gmail.com>
2022-10-21 14:18:49 -0700

    Added login validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,10 +4,38 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+// backend/routes/api/session.js
+// ...
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// ...
+
+
 const router = express.Router();
 
+
+  // backend/routes/api/session.js
+// ...
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+
+// backend/routes/api/session.js
+// ...
+
+// Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -28,7 +56,6 @@ router.post(
       });
     }
   );
-
 
 // backend/routes/api/session.js
 // ...
@@ -66,5 +93,8 @@ router.get(
   );
 
   // ...
+
+
+
 
 module.exports = router;
