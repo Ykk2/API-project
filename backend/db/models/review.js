da959b4c365fd58b4a8f5f596d21821ccc057348
Richard <richardkwon2@gmail.com>
2022-10-30 16:30:20 -0700

    fixing alias again

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Review.belongsTo(models.User, {foreignKey: 'userId'})
       Review.belongsTo(models.Spot, {foreignKey: 'spotId'})
-      Review.hasMany(models.ReviewImage, {foreignKey: 'reviewId', as:'ReviewImages', onDelete: 'CASCADE'})
+      Review.hasMany(models.ReviewImage, {foreignKey: 'reviewId', as:"ReviewImages", onDelete: 'CASCADE'})
     }
   }
   Review.init({
