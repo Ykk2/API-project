47bee2e8bece42ffab29c919c09c974c2c838f28
Richard <richardkwon2@gmail.com>
2022-10-30 16:44:54 -0700

    maybe

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -77,7 +77,7 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
         attributes: {
             include: [[sequelize.fn('COUNT', sequelize.col('ReviewImages.id')), 'count']]
         },
-        group: ['reviews.id'],
+        group: ['ReviewImages.id'],
         raw: true
     })
 
