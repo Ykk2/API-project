e16f165aaa556ac375b9a3095c0e2a776faed6c1
Richard <richardkwon2@gmail.com>
2022-10-30 15:24:01 -0700

    adding few more things to groupby parameter

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -73,7 +73,7 @@ router.get('/:spotId', async (req, res) => {
                       [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgStarRating']]
 
         },
-        group: ['Spot.id', 'SpotImages.id'],
+        group: ['Spot.id', 'SpotImages.id', 'User.id'],
     })
 
     if (!spot) {
